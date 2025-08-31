3ac87fb48f91cf60029a6d54b639e1b33b0f3ee0
Wairia0 <Joseph.Wairia@strathmore.edu>
2025-08-31 23:37:12 +0300

    Intro

---
+++ 01_intro_to_python/01_input_output.py
@@ -0,0 +1,5 @@
+name = input ("Enter your name")
+num = int(input("Enter a number ")) #type casting
+
+print (f"My name is {name} and the number is {num}")
+print ("My name is "+name+" and the number is "+num)
